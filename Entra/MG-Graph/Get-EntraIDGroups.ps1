9e3eb0d1d37aa355b57f621e8aa9985e3f14fe59
AnkitG365 <144297747+AnkitG365@users.noreply.github.com>
2024-08-22 14:52:54 -0500

    Create Get-EntraIDGroups.ps1

---
+++ Entra/MG-Graph/Get-EntraIDGroups.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    Retrieves all groups from Entra ID.
+
+.DESCRIPTION
+    This script retrieves all groups from Entra ID and outputs their display name, group type, and membership type.
+
+.EXAMPLE
+    .\Get-EntraIDGroups.ps1
+
+.NOTES
+    Author: Ankit Gupta
+    GitHub: https://github.com/ankytgupta
+
+    # Authentication can be done using a certificate (as below) or using a client secret:
+    # Connect-MgGraph -ClientId $AppId -TenantId $TenantId -ClientSecret $ClientSecret
+#>
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$AppId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$TenantId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$CertThumbprint
+)
+
+# Connect using certificate-based authentication
+Connect-MgGraph -ClientId $AppId -TenantId $TenantId -CertificateThumbprint $CertThumbprint -Scopes Group.Read.All
+
+# Retrieve all groups
+Get-MgGroup -All | Select-Object DisplayName, GroupTypes, MembershipRule | Format-Table
