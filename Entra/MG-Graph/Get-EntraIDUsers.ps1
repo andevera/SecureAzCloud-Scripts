5c1191cda60cca989c46dbababbe7a73819b7cbc
AnkitG365 <144297747+AnkitG365@users.noreply.github.com>
2024-08-22 14:52:42 -0500

    Create Get-EntraIDUsers.ps1

---
+++ Entra/MG-Graph/Get-EntraIDUsers.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    Retrieves all users from Entra ID.
+
+.DESCRIPTION
+    This script retrieves all users from Entra ID and outputs their display name, user principal name (UPN), and account status.
+
+.EXAMPLE
+    .\Get-EntraIDUsers.ps1
+
+.NOTES
+    Author: Ankit Gupta
+    GitHub: https://github.com/ankytgupta
+
+    # Authentication can be done using a certificate (as below) or using a client secret:
+    # Connect-MgGraph -ClientId $AppId -TenantId $TenantId -ClientSecret $ClientSecret
+#>
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$AppId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$TenantId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$CertThumbprint
+)
+
+# Connect using certificate-based authentication
+Connect-MgGraph -ClientId $AppId -TenantId $TenantId -CertificateThumbprint $CertThumbprint -Scopes User.Read.All
+
+# Retrieve all users
+Get-MgUser -All | Select-Object DisplayName, UserPrincipalName, AccountEnabled | Format-Table
